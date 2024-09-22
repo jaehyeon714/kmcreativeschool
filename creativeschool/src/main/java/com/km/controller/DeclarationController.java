0abce7bf55117b18720a3432a1b50af2fc98b230
Beakdonghund <bangdong06@gmail.com>
2024-09-22 17:27:47 +0900

    예진_report 완

--- creativeschool/src/main/java/com/km/controller/DeclarationController.java
+++ creativeschool/src/main/java/com/km/controller/DeclarationController.java
@@ -4,7 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/decalration")
+@RequestMapping("/declaration")
 public class DeclarationController {
-
+	
+	@RequestMapping("/requestdeclaration.km")
+	public String reportinfo() {
+		return "report/reporter";
+	}
 }
