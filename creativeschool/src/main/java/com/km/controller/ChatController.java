85295844d1fdb4bc1f47004ff4bb351aba229f1a
Eunjeong Choi <ejchoi@kmh.hs.kr>
2024-09-29 17:28:28 +0900

    bs_채팅작업시작

--- creativeschool/src/main/java/com/km/controller/ChatController.java
+++ creativeschool/src/main/java/com/km/controller/ChatController.java
@@ -1,13 +1,26 @@
 package com.km.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.km.model.dto.Police;
+import com.km.model.service.PoliceService;
+
+import lombok.RequiredArgsConstructor;
+
 @Controller
+@RequiredArgsConstructor
 public class ChatController {
 	
+	private final PoliceService service;
+	
 	@RequestMapping("/chat/livechatlist.km")
-	public String chatList() {
+	public String chatList(Model m) {
+		List<Police> polices=service.selectPoliceAll();
+		m.addAttribute("polices", polices);
 		return "chat/chatlist";
 	}
 	
