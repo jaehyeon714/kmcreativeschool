4059d1d996bed2fc38f061e19a8dd1bcaf80c796
Eunjeong Choi <ejchoi@kmh.hs.kr>
2024-10-05 16:44:23 +0900

    권민서_ 경찰등록 구현

--- creativeschool/src/main/java/com/km/model/dao/PoliceDaolmpl.java
+++ creativeschool/src/main/java/com/km/model/dao/PoliceDaolmpl.java
@@ -7,6 +7,8 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
 import com.km.model.dto.Police;
+import com.km.model.dto.PoliceAttachment;
+import com.km.model.dto.PoliceStation;
 
 @Repository
 public class PoliceDaolmpl implements PoliceDao{
@@ -20,10 +22,27 @@ public class PoliceDaolmpl implements PoliceDao{
 	public int insertPolice(SqlSession session, Police police) {
 		 return session.insert("police.insertPolice", police);
 	}
+	
+	
 
 	@Override
 	public List<Map> selectPoliceAll(SqlSession session) {
 		return session.selectList("police.selectPoliceAll");
+	}
+
+	@Override
+	public PoliceStation selectPoliceStationByName(SqlSession session, String name) {
+		return session.selectOne("police.selectPoliceStationByName", name);
+	}
+
+	@Override
+	public int insertPoliceStation(SqlSession session, PoliceStation policeStation) {
+		return session.insert("police.insertPoliceStation", policeStation);
+	}
+
+	@Override
+	public int insertPoliceAttachment(SqlSession session, PoliceAttachment attachment) {
+		return session.insert("police.insertPoliceAttachment", attachment);
 	}
 	
 	
